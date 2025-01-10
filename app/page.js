742253a39fcc3678c74fb03e5f11d7b9df04882f
Hazko-stack <hazzakao@gmail.com>
2025-01-10 21:42:53 +0700

    ubah animasii

--- app/page.js
+++ app/page.js
@@ -21,8 +21,7 @@ export default function Home() {
         <img
           src="/images/planet.png"
           alt="Planet"
-          loading="lazy"
-          className="w-1/2 h-auto motion-scale-in-[0.5] motion-translate-x-in-[-120%] motion-translate-y-in-[-60%] motion-opacity-in-[33%] motion-rotate-in-[-1080deg] motion-blur-in-[10px] motion-delay-[0.38s]/scale motion-duration-[0.38s]/opacity motion-duration-[1.20s]/rotate motion-duration-[0.15s]/blur motion-delay-[0.60s]/blur motion-ease-spring-bouncier "
+          className="w-1/2 h-auto animate-pulse"
         />
       </div>
 
